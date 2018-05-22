2e5709e30de70b99aa946791d37cb2ae27fc5545
uetsu840 <vd7t-ktgw@asahi-net.or.jp>
2018-05-22 23:16:47 +0900

    added description

--- Angular/myiot-app-station-control/src/app/signal.ts
+++ Angular/myiot-app-station-control/src/app/signal.ts
@@ -1,6 +1,7 @@
 export class Signal {
     id: number;
     name: string;
+    description: string;
     position: number;
     indication: number;
     display_style_red:    string;
--- Angular/myiot-app-station-control/src/app/switch.ts
+++ Angular/myiot-app-station-control/src/app/switch.ts
@@ -1,6 +1,7 @@
 export class Switch {
     id: number;
     name: string;
+    description: string;
     position: number;
     display_style_normal:  string;
     display_style_reverse: string;

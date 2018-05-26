f9bc076b9fca430713ab3e0cbb57fe181ba3d20a
uetsu840 <vd7t-ktgw@asahi-net.or.jp>
2018-05-26 20:27:39 +0900

    changed single signal to property binding

--- Angular/myiot-app-station-control/src/app/dashboard/control-panel/control-panel.component.ts
+++ Angular/myiot-app-station-control/src/app/dashboard/control-panel/control-panel.component.ts
@@ -23,6 +23,39 @@ export class ControlPanelComponent implements OnInit {
     text1_pos: { x: 850, y: 440 },
     text2_pos: { x: 840, y: 525 },
   };
+  signal_1L: SignalDisplay = {
+    signal_pos: { x: 250, y: 320 },
+    rotate: 180,
+    name1: '1L',
+    name2: '',
+    text1_pos: { x: 150, y: 260 },
+    text2_pos: { x: 0, y: 0 }
+  };
+  signal_2L: SignalDisplay = {
+    signal_pos: { x: 250, y: 490 },
+    rotate: 180,
+    name1: '2L',
+    name2: '',
+    text1_pos: { x: 150, y: 520 },
+    text2_pos: { x: 0, y: 0 }
+  };
+  signal_3R: SignalDisplay = {
+    signal_pos: { x: 650, y: 270 },
+    rotate: 0,
+    name1: '3R',
+    name2: '',
+    text1_pos: { x: 700, y: 260 },
+    text2_pos: { x: 0, y: 0 }
+  };
+  signal_4R: SignalDisplay = {
+    signal_pos: { x: 650, y: 440 },
+    rotate: 0,
+    name1: '4R',
+    name2: '',
+    text1_pos: { x: 700, y: 520 },
+    text2_pos: { x: 0, y: 0 }
+  };
+
 
 
   constructor() { }
--- Angular/myiot-app-station-control/src/app/dashboard/control-panel/single-signal/single-signal.component.ts
+++ Angular/myiot-app-station-control/src/app/dashboard/control-panel/single-signal/single-signal.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { SignalDisplay } from '../signal-display';
 
 @Component({
-  selector: 'app-single-signal',
+  selector: '[app-single-signal]',
   templateUrl: './single-signal.component.html',
   styleUrls: ['./single-signal.component.css']
 })
 export class SingleSignalComponent implements OnInit {
+  @Input() signal: SignalDisplay;
 
   constructor() { }
 
